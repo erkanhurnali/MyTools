6eb04f5c9acf8d898a235b8fc19b2306d2073dd6
Erkan Hürnalı <erkanhurnali@gmail.com>
2024-01-15 11:10:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,16 @@ import flet as ft
 from myAlarms import AlarmButton
 from myMouseButtons import RightButton
 # import utils as utils
-from myShrcuts import ToogleButton
-from flet import Page,  Row, icons, colors, Column
-# import time
+from flet import Page,  Row, icons, colors
 from utils.MouseMonitoring import MouseMonitoring
 
 def main(page: Page):
 
-    page.title = "MyTools - Tiny"
+    page.title = "TiniMini"
     page.window_height = 87
     page.window_top = 70
     page.window_left = 120
-    page.window_width = 165
+    page.window_width = 10
     page.window_always_on_top = True
     page.padding = 0
     page.window_resizable = False
@@ -26,6 +24,7 @@ def main(page: Page):
 
     main_row = Row(
         spacing=0,
+        alignment = ft.MainAxisAlignment.CENTER
     )
     rightButton = RightButton(
         icon=icons.MOUSE_OUTLINED,
@@ -38,13 +37,6 @@ def main(page: Page):
         rightButton,
     )
 
-    main_row.controls.append(ToogleButton(
-        icon=icons.CHANGE_CIRCLE_OUTLINED,
-        selected_icon=icons.CHANGE_CIRCLE,
-        ipucu="Change Active Program", 
-        color=colors.CYAN
-    ))
-
     main_row.controls.append(AlarmButton(
         icon=icons.NOTIFICATION_IMPORTANT_OUTLINED,
         selected_icon=icons.NOTIFICATION_IMPORTANT,
@@ -52,9 +44,7 @@ def main(page: Page):
         color=colors.AMBER,
     ))
 
-
-    main_col = Column(controls=[main_row,])
-    page.add(main_col)
+    page.add(main_row)
 
     MouseMonitoring(rb=rightButton)
     
